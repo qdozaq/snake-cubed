9133d3e903b829e1edd6f7101b2b3c981c964039
qdozaq <doza@indiginis.com>
2019-12-21 12:29:11 -0500

    fix rotation for touch events

--- src/Rotation.tsx
+++ src/Rotation.tsx
@@ -6,7 +6,6 @@ type Props = {
   children: React.ReactNode;
 };
 
-//TODO: figure out why dragging doesn't work on mobile
 export default ({ children }: Props) => {
   const [{ rotation }, set] = useSpring(() => ({
     rotation: [0, 0, 0]
@@ -15,12 +14,8 @@ export default ({ children }: Props) => {
   const dampen = 100;
   const bindDrag = useDrag(
     ({ movement: [mx, my], offset: [ox, oy], down, ...rest }) => {
-      console.log(rest);
-      if (down) {
-        console.log({ mx, my, ox, oy });
-        const [x, y] = [ox / dampen, oy / dampen];
-        set({ rotation: [Math.abs(y) <= 1 ? y : Math.sign(y), x, 0] });
-      }
+      const [x, y] = [ox / dampen, oy / dampen];
+      set({ rotation: [Math.abs(y) <= 1 ? y : Math.sign(y), x, 0] });
     },
     { pointerEvents: true }
   );
