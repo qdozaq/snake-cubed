9d4548dfe9c29997cad43f52db2b545cc5a73846
qdozaq <doza@indiginis.com>
2019-12-24 17:22:21 -0500

    implement random color

--- src/Cube.tsx
+++ src/Cube.tsx
@@ -15,32 +15,28 @@ const Cube = ({ size }: Props) => {
         />
         <meshPhysicalMaterial
           attach="material"
-          color="black"
+          color="#241229"
           roughness={0.8}
           clearcoat={0.1}
           reflectivity={0.2}
           metalness={0.3}
-          transparent={true}
-          opacity={0.7}
+          // transparent={true}
+          // opacity={0.}
           wireframe={true}
         />
       </mesh>
-      {/* <mesh castShadow={true}>
+      <mesh castShadow={true}>
         <boxBufferGeometry
           attach="geometry"
-          args={[size - 1, size - 1, size - 1]}
+          args={[size - 0.01, size - 0.01, size - 0.01]}
         />
-        <meshPhysicalMaterial
+        <meshBasicMaterial
           attach="material"
-          color="pink"
-          roughness={0.8}
-          clearcoat={0.1}
-          reflectivity={0.2}
-          metalness={0.3}
+          color="#ff80ae"
           transparent={true}
-          opacity={0.5}
+          opacity={0.4}
         />
-      </mesh> */}
+      </mesh>
     </group>
   );
 };
--- src/Snake.tsx
+++ src/Snake.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Vector3, Euler, BoxBufferGeometry } from 'three';
+import { Vector3, Euler, BoxBufferGeometry, Color } from 'three';
 import { SubdivisionModifier } from 'three/examples/jsm/modifiers/SubdivisionModifier';
 
 import { PositionNode } from './PositionNode';
-const modifier = new SubdivisionModifier(2);
-const segments = 3;
+const modifier = new SubdivisionModifier(1);
+const segments = 4;
 const segmentSize = 1 - 0.05;
 const cubeGeometry = new BoxBufferGeometry(
   segmentSize,
@@ -17,15 +17,21 @@ const cubeGeometry = new BoxBufferGeometry(
 // const smooth = cubeGeometry;
 const smooth = modifier.modify(cubeGeometry);
 
+const randomColor = () => '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+
+const startColor = new Color('#ab3');
+
 type Props = {
   body: PositionNode[];
 };
 
+const colorMemo = { 0: startColor };
+
 export default function Snake({ body }: Props) {
   return (
     <>
-      {body.map(node => (
-        <SnakeSegment position={node.vector} rotation={node.euler} />
+      {body.map((node, i) => (
+        <SnakeSegment position={node.vector} rotation={node.euler} index={i} />
       ))}
     </>
   );
@@ -34,16 +40,23 @@ export default function Snake({ body }: Props) {
 type SnakeSegmentProps = {
   position: [number, number, number] | Vector3;
   rotation: Euler;
+  index: number;
 };
 
-export const SnakeSegment = (props: SnakeSegmentProps) => {
+export const SnakeSegment = ({ index, ...props }: SnakeSegmentProps) => {
+  if (!colorMemo[index]) colorMemo[index] = randomColor();
+
   return (
     <mesh {...props} castShadow={true} geometry={smooth}>
       {/* <boxBufferGeometry
         attach="geometry"
         args={[segmentSize, segmentSize, 0.5]}
       /> */}
-      <meshPhysicalMaterial attach="material" color="#966BC2" />
+      <meshPhysicalMaterial
+        attach="material"
+        color={colorMemo[index]}
+        roughness={1}
+      />
     </mesh>
   );
 };
