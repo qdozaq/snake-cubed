ee6005a8f7c75dedbb13418ca385f12b219a18c6
Paul Mendoza <doza@indiginis.com>
2020-02-20 20:48:42 -0500

    cmon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import ReactGA from 'react-ga';
-const trackingCode = 'UA-131385998-2';
-ReactGA.initialize(trackingCode);
 
-// import initGA from './analytics';
 import Game from './game/Game';
 import themes from './themes';
 import { MenuContainer, MenuButton } from './components/Menu';
 import GlobalStyle from './GlobalStyle';
 import HowToPage from './howto/HowToPage';
+
+const trackingCode = 'UA-131385998-2';
+ReactGA.initialize(trackingCode);
 
 const DEFAULT_SIZE = 3;
 const DEFAULT_SPEED = 2;
